b15fda34a9a98140840241f76d4a6207636167ac
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-12-06 15:29:09 -0300

    feat(req 02): criação da função para pegar todos os tipos de trabalhos

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -38,7 +38,12 @@ def get_unique_job_types(path: str) -> List[str]:
     list
         List of unique job types
     """
-    raise NotImplementedError
+    # raise NotImplementedError
+    jobs = read(path)
+    unique_jobs = set()
+    for job in jobs:
+        unique_jobs.add(job["job_type"])
+    return list(unique_jobs)
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
