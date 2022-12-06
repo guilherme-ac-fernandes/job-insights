d884e641060410eaab041a9bb3a65c41a5e2f6fa
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-12-06 18:05:10 -0300

    feat(req 10): implementação do teste para função counter de palavras

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,16 @@
-# from src.pre_built.counter import count_ocurrences
+import pytest
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    assert count_ocurrences("data/jobs.csv", 'python') == 1639
+    assert count_ocurrences("data/jobs.csv", 'PYTHON') == 1639
+    assert count_ocurrences("data/jobs.csv", 'JavaScriPt') == 122
+    assert count_ocurrences("data/jobs.csv", 'JAvAscrIPT') == 122
+    assert count_ocurrences("data/jobs.csv", 'salário') == 0
+    assert count_ocurrences("data/jobs.csv", 'título') == 0
+
+
+def test_counter_fail_file():
+    with pytest.raises(FileNotFoundError):
+        count_ocurrences("data/jobs.json", 'python')
