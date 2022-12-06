3e1308071fe84b09a64e7decb9da8cec42e72819
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-12-06 19:14:19 -0300

    feat(req 11): implementação do teste para função de leitura dos arquivos em português

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,27 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+import random
+from src.pre_built.brazilian_jobs import read_brazilian_file
+
+
+FILE_PATH = 'tests/mocks/brazilians_jobs.csv'
+
+index = random.randint(1, len(read_brazilian_file(FILE_PATH)) - 1)
+
+
+def test_brazilian_jobs_rename_keys():
+    assert "title" in read_brazilian_file(FILE_PATH)[index]
+    assert "salary" in read_brazilian_file(FILE_PATH)[index]
+    assert "type" in read_brazilian_file(FILE_PATH)[index]
+
+
+def test_brazilian_jobs_removed_keys():
+    assert "titulo" not in read_brazilian_file(FILE_PATH)[index]
+    assert "salario" not in read_brazilian_file(FILE_PATH)[index]
+    assert "tipo" not in read_brazilian_file(FILE_PATH)[index]
 
 
 def test_brazilian_jobs():
-    pass
+    assert isinstance(read_brazilian_file(FILE_PATH), list)
+    assert isinstance(read_brazilian_file(FILE_PATH)[index], dict)
+    assert len(read_brazilian_file(FILE_PATH)) == 15
+    test_brazilian_jobs_rename_keys()
+    test_brazilian_jobs_removed_keys()
