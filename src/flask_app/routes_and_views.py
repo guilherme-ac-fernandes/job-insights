ac8d361226228e016ecfa569b0c0014ee2eacc6a
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-12-06 22:58:39 -0300

    feat(req 13): criação da rota de /job/index para renderizar apenas um emprego

--- src/flask_app/routes_and_views.py
+++ src/flask_app/routes_and_views.py
@@ -20,6 +20,7 @@ from src.flask_app.more_insights import (
     slice_jobs,
     get_int_from_args,
     build_jobs_urls,
+    get_job,
 )
 
 bp = Blueprint("client", __name__, template_folder="templates")
@@ -77,6 +78,14 @@ def list_jobs():
     }
 
     return render_template("list_jobs.jinja2", ctx=ctx)
+
+
+@bp.route("/job/<index>")
+# @bp.route("/job/<int:index>")
+def job(index):
+    jobs = read(path="data/jobs.csv")
+    job_index = get_job(jobs, int(index))
+    return render_template("job.jinja2", job=job_index)
 
 
 def init_app(app: Flask):
