b99015e6562c0db1ebf359fc64e7af720b94c8e4
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-12-07 16:26:24 -0300

    refactor: padronização do teste com file_path

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -3,12 +3,14 @@ from src.pre_built.counter import count_ocurrences
 # Resolução desenvolvida com o auxílio dos instrutores de Ciência
 # da Computação: Marco Mialaret Júnior e Rodrigo Coin Curvo
 
+FILE_PATH = "data/jobs.csv"
+
 
 def test_counter():
-    assert count_ocurrences("data/jobs.csv", 'Python') == 1639
-    assert count_ocurrences("data/jobs.csv", 'python') == 1639
-    assert count_ocurrences("data/jobs.csv", 'PYTHON') == 1639
-    assert count_ocurrences("data/jobs.csv", 'Javascript') == 122
-    assert count_ocurrences("data/jobs.csv", 'JavaScriPt') == 122
-    assert count_ocurrences("data/jobs.csv", 'salário') == 0
-    assert count_ocurrences("data/jobs.csv", 'título') == 0
+    assert count_ocurrences(FILE_PATH, "Python") == 1639
+    assert count_ocurrences(FILE_PATH, "python") == 1639
+    assert count_ocurrences(FILE_PATH, "PYTHON") == 1639
+    assert count_ocurrences(FILE_PATH, "Javascript") == 122
+    assert count_ocurrences(FILE_PATH, "JavaScriPt") == 122
+    assert count_ocurrences(FILE_PATH, "salário") == 0
+    assert count_ocurrences(FILE_PATH, "título") == 0
